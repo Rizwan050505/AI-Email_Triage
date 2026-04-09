0b6ba5e8c454a3460c4118ec20d30cf2d3c6e995
Rizwan050505 <rizwansheikh.050505@gmail.com>
2026-04-10 00:40:22 +0530

    Fix entrypoint function name

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 import uvicorn
 from backend.main import app
 
-def start():
+def main():
     uvicorn.run("backend.main:app", host="0.0.0.0", port=7860)
 
-if __name__ == "__main__":
-    start()
+if __name__ == '__main__':
+    main()
